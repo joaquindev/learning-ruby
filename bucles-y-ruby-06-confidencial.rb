5f8dbebc46dd1b4054a4f06d65c18d0f330f3a16
JoaquinDev <joaquin@isla.io>
2014-01-29 09:19:07 +0100

    Create file with the section of the tutorial
    Create file with the section of the tutorial
    
    Contact: joaquin@isla.io

---
+++ bucles-y-ruby-06-confidencial.rb
@@ -0,0 +1,14 @@
+puts "Texto en el cual buscar"
+texto = gets.chomp
+puts "Palabra a modificar: "
+modificar = gets.chomp
+
+palabras = texto.split(" ")
+
+palabras.each do |palabra|
+  if palabra != modificar
+    print palabra + " " 
+  else
+    print "CONFIDENCIAL "
+  end
+end
